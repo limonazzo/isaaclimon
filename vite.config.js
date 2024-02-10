bd0c2a40df209c74ff09fc7f241c1cc966433438
Limon <limonazzo@outlook.com>
2024-02-09 22:43:21 -0600

    fix: build to prod

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { resolve } from 'path'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
 export default {
-    root: resolve(__dirname, 'src'),
-    build: { outDir: resolve(__dirname, 'dist') },
+    root: root,
+    build: { 
+      outDir: outDir,
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(root, 'index.html'),
+          es: resolve(root,'es.html')
+        }
+      }
+    },
     plugins: [
         ViteMinifyPlugin({}),
     ],
